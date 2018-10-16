6a19888ec056f08afd8fa536ccd8756a080af55b
jasmine wei <jasminewei0224@gmail.com>
2018-10-15 20:42:01 -0400

    wenhlkwetn

--- grid_exercise.js
+++ grid_exercise.js
@@ -2,7 +2,116 @@ for (var i = 0; i < 10; i = i + 1) {
     for (var j = 0; j < 10; j = j + 1) {
         console.log("Row " + i + ", Column " + j);
 
+        if (j < 5) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
         if (i < 5) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+
+        if (i == j) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        if (j < 1+i) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        if (i == 0, 2, 4, 6, 8) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        if (j > 1+i) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        if (j < 3) {
             console.log("   pink");
             document.write("<div class='box pink'></div>");
         } else {
